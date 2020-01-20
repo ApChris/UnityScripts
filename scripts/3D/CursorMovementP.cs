5b1889b155766fcba7dd7e09b541b51a7b4d9e7f
Christoforos Apostolopoulos <56033338+ApChris@users.noreply.github.com>
2020-01-20 21:46:40 +0200

    Update file , Rename it and
    Cursor Movement, 3D

---
+++ scripts/3D/CursorMovementP.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class CursorMovementP : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    Ray lastRay;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        bool isHit = Physics.Raycast(ray, out hit);
+        if(isHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+}
